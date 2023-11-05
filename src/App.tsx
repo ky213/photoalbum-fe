3a94d0cce25dfd7c1339660ac0cb6a115b1022c5
YAAQOUB KEDDAOUI <y.keddaoui@gmail.com>
2023-11-05 09:16:58 +0100

    added error boundry

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { Route, Routes } from "react-router-dom";
 
 import { MainLayout } from "./layouts";
 import { HomePage, LoginPage, ProfilePage, RegiterPage, NotFound } from "./pages";
+import { ErrorBoundry } from "./components";
 
 function App() {
   return (
     <MainLayout>
-      <Routes>
-        <Route path="/" Component={HomePage} />
-        <Route path="/login" Component={LoginPage} />
-        <Route path="/register" Component={RegiterPage} />
-        <Route path="/profile" Component={ProfilePage} />
-        <Route path="*" Component={NotFound} />
-      </Routes>
+      <ErrorBoundry>
+        <Routes>
+          <Route path="/" Component={HomePage} />
+          <Route path="/login" Component={LoginPage} />
+          <Route path="/register" Component={RegiterPage} />
+          <Route path="/profile" Component={ProfilePage} />
+          <Route path="*" Component={NotFound} />
+        </Routes>
+      </ErrorBoundry>
     </MainLayout>
   );
 }
